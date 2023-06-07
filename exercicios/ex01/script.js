d029385d21ca7e963ef9c5eddd179107641d85e0
Thiago Cavalcanti <thiago.profmat@gmail.com>
2023-06-07 19:20:18 -0300

    Work: estudo 1 do exercício 1

--- exercicios/ex01/script.js
+++ exercicios/ex01/script.js
@@ -1,22 +1,24 @@
-var a = window.document.getElementById('imagens')
-var res = document.getElementById('res_1')
-
-
+var a = window.document.getElementById('imagem')
+var res = document.getElementById('res')
 
 a.addEventListener('mouseenter',entrar)
 a.addEventListener('click',clicar)
 a.addEventListener('mouseout',sair)
 
+
+
+
+
+
 function clicar(){
-    res.innerText = 'Clicou2'
-    
+    res.innerHTML = 'Clicou na imagem da manhã'
 }
 
 function entrar(){
-    res.innerText = 'Entrou'
+    res.innerHTML = 'Entrou na imagem da manhã'
+    
 }
 
 function sair(){
-    res.innerText = ''
-    
+    res.innerHTML = ''
 }
