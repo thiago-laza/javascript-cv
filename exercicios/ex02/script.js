8a6fbd93548c6cad7faa58f7622c86e57481a128
thiago <thiago.profmat@gmail.com>
2023-06-14 23:22:40 -0300

    Home: ex02 - function soma e valores NUMBER de input

--- exercicios/ex02/script.js
+++ exercicios/ex02/script.js
@@ -1,8 +1,19 @@
-let valor1 = document.getElementById('valor-1')
-let valor2 = document.getElementById('valor-2')
+var valor1 = document.querySelector('input#valor-1')
+var valor2 = document.querySelector('input#valor-2')
 
-let num1 = Number(valor1)
-let num2 = Number(valor2)
+let soma = document.getElementById('adicao')
+let res = document.getElementById('res')
+
+
+
+
+soma.addEventListener('click',clicar)
+
+function clicar(){
+    alert(`${Number(valor1.value)+Number(valor2.value)}`)
+    res.innerHTML = `${Number(valor1.value)+Number(valor2.value)}`
+}
+
 
 
 
